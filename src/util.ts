04621bf815bca6059c2a7b9e7c11a15cb563fa0d
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2019-03-31 21:42:10 -0700

    Simplify + annotate util

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@
  * Created by anth on 21.05.2017.
  */
 
-import {Buffer} from 'buffer';
+import { Buffer } from 'buffer';
 import * as zlib from 'zlib';
 
 
@@ -14,14 +14,7 @@ const toExport = {
    */
   decode: {
     0: (str: string) => { // Version 0
-      let data = null;
-      try {
-        data = JSON.parse(zlib.inflateSync(Buffer.from(str.slice(1), 'base64')).toString('utf8'));
-      } catch (e) {
-        throw e;
-      }
-
-      return data;
+      return JSON.parse(zlib.inflateSync(Buffer.from(str.slice(1), 'base64')).toString('utf8')) as object;
     },
     latest: null
   },
@@ -32,7 +25,7 @@ const toExport = {
    * @returns {string} object encoded in Factorio .15 format
    */
   encode: {
-    0: (obj: any) => { // Version 0
+    0: (obj: object) => { // Version 0
       return '0' + zlib.deflateSync(JSON.stringify(obj)).toString('base64');
     },
     latest: null
